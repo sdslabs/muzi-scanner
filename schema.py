a480278ebdac09f95f1b44a8bf3569698f98b2e2
Gautham Goli <goligautham@gmail.com>
2017-01-29 18:48:24 +0530

    Update schema to use text field instead of string fields

--- schema.py
+++ schema.py
@@ -1,7 +1,7 @@
 __author__ = 'gautham'
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String,Text, ForeignKey, Float, DateTime, func, UniqueConstraint
+from sqlalchemy import Column, Integer, String,Text, ForeignKey, Float, DateTime, func, UniqueConstraint, Boolean
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.schema import ForeignKeyConstraint
 import datetime
@@ -13,9 +13,9 @@ class Band(Base):
     __tablename__ = 'bands'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(255))
-    language = Column(String(255))
-    info = Column(Text())
+    name = Column(Text)
+    language = Column(String(100))
+    info = Column(Text)
 
 
 class Year(Base):
@@ -27,18 +27,18 @@ class Year(Base):
 class Genre(Base):
     __tablename__ = 'genres'
     id = Column(Integer, primary_key=True)
-    name = Column(String(255))
+    name = Column(Text)
 
 
 class Album(Base):
     __tablename__ = 'albums'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(255))
-    language = Column(String(255))
-    info = Column(Text())
+    name = Column(Text)
+    language = Column(String(100))
+    info = Column(Text)
     band_id = Column(Integer)#, ForeignKey('bands.id'))
-    band_name = Column(String(255))#, ForeignKey('bands.name'))
+    band_name = Column(String(100))#, ForeignKey('bands.name'))
 
     band = relationship('Band', backref=backref('albums'))
 
@@ -51,19 +51,20 @@ class Track(Base):
     __tablename__ = 'tracks'
 
     id = Column(Integer, primary_key=True)
-    file = Column(String(1024))
-    title = Column(String(255))
+    file = Column(Text)
+    title = Column(Text)
     album_id = Column(Integer, ForeignKey('albums.id'))
     band_id = Column(Integer, ForeignKey('bands.id'))
     genre_id = Column(Integer, ForeignKey('genres.id'))
-    artist = Column(String(255))
+    artist = Column(Text)
     # track number
     track = Column(Integer)
     plays = Column(Integer, default=0)
     year_id = Column(Integer)
     length = Column(Integer)
-    creation_time = Column(DateTime, default=datetime.datetime.utcnow)
-
+    lyrics = Column(Text)
+    creation_time = Column(Integer, default=0)
+    status_add = Column(Boolean)
 
     album = relationship('Album', backref=backref('tracks'))
     genre = relationship('Genre', backref=backref('tracks'))
